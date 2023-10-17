d2e2a46230c0cfb48e9f5b8c04a1ace112905667
Oluwatosin Daniel Owolabi <danielty2k@gmail.com>
2023-10-17 12:04:04 +0100

    add dotenv

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,16 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from './auth/auth.module';
 import { PostsModule } from './posts/posts.module';
 import { LikesModule } from './likes/likes.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+    }),
     MongooseModule.forRoot(
-      `mongodb+srv://Odevtools007:QNQczaC83JTeI39a@cluster0.1bhjq.mongodb.net/nestDB?retryWrites=true&w=majority`,
+      `mongodb+srv://${process.env.USER_NAME}:${process.env.DB_PASSWORD}@cluster0.1bhjq.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`,
     ),
     AuthModule,
     UsersModule,
