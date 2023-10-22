a60f06466a67534c988c21053d714e6b9cad9ead
Oluwatosin Daniel Owolabi <danielty2k@gmail.com>
2023-10-22 03:41:34 +0100

    update

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 import { NextFunction, Request, Response } from 'express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   app.use(function (request: Request, response: Response, next: NextFunction) {
     response.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
     next();
