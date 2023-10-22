74aeb1bc7535a86c6ea7365ee14e5c9ab42dcdce
Oluwatosin Daniel Owolabi <danielty2k@gmail.com>
2023-10-22 18:15:09 +0100

    update config

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { NextFunction, Request, Response } from 'express';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: true,
+    origin: 'http://localhost:3000',
     methods: 'GET, HEAD, PUT, PATCH, POST, DELETE',
     preflightContinue: false,
     credentials: true,
