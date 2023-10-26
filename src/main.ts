465a83688b9afeb8d0e419aaa808cd86b43e241c
Oluwatosin Daniel Owolabi <danielty2k@gmail.com>
2023-10-26 05:32:19 +0100

    update config

--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: 'http://localhost:3000',
-    methods: 'GET, HEAD, PUT, PATCH, POST, DELETE',
-    preflightContinue: false,
+    methods: ['GET, HEAD, PATCH, POST, DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
-    allowedHeaders: 'Authorization',
+    preflightContinue: false,
   });
   // app.use(function (request: Request, response: Response, next: NextFunction) {
   //   response.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
