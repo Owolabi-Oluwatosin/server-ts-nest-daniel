2afb949ba19e5077416d02cd245d916cf37e8495
Oluwatosin Daniel Owolabi <danielty2k@gmail.com>
2023-10-26 07:05:45 +0100

    update cors

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,28 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { NextFunction, Request, Response } from 'express';
+import cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // app.use(
+  //   cors({
+  //     origin: 'https://nest-client-daniel.vercel.app/',
+  //     credentials: true,
+  //   }),
+  // );
   app.enableCors({
     origin: 'https://nest-client-daniel.vercel.app',
-    methods: ['GET, HEAD, PATCH, POST, DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
+    methods: ['GET, HEAD, OPTION, PATCH, POST, DELETE'],
+    allowedHeaders: [
+      'Origin',
+      'X-Requested-With',
+      'Content-Type',
+      'Accept',
+      'X-Access-Token',
+      'Authorization',
+    ],
     credentials: true,
-    preflightContinue: false,
+    preflightContinue: true,
   });
   // app.use(function (request: Request, response: Response, next: NextFunction) {
   //   response.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
